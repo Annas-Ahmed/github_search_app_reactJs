a44860be6be54c0f78e4d77a0022d366ac993984
Annas-Ahmed <annasahmed1609@gmail.com>
2022-09-25 10:51:46 +0500

    gihub search app complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css'
 
 function App() {
+  const [inputValue, setInputValue] = useState('')
+  const [userData, setUserData] = useState("");
+  const [searchUser, setSearchUser] = useState('')
+  const [callApi, setCallApi] = useState(true)
+  useEffect(() => {
+    axios.get(`https://api.github.com/users/${inputValue ? inputValue : "annas-ahmed"}`)
+      .then((res) => {
+        setUserData(res.data);
+        setCallApi(true)
+      })
+      .catch((error) => {
+        console.log(error);
+        setCallApi(false)
+      })
+  }, [searchUser]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchUser(!searchUser)
+    console.log(inputValue)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <section className='main-section'>
+        <h1>
+          github search bar
+        </h1>
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder='search username'>
+            </input>
+          </form>
+          {
+            callApi ?
+              <section>
+                <div>
+                  <img src={userData.avatar_url} alt='user' />
+                </div>
+                <li>Name : {userData.name}</li>
+                <li>bio : {userData.bio}</li>
+                <li>followers : {userData.followers}</li>
+                <li>following : {userData.following}</li>
+                <li>repositries : {userData.public_repos}</li>
+              </section>
+              :
+              <img className='notfound-image' src={require('./Assets/error.png')} alt='not found' />
+          }
+        </section>
+      </section>
+    </>
+
+  )
 }
 
 export default App;
